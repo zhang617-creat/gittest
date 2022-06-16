4951076b6d2afb70e0fa1c3d31a50d39986270ed
zlh <193911857@qq.com>
2022-06-16 18:27:37 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author zlh
+ * @create 2022-${HOME}-16 下午 6:23
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
