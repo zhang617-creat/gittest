a886156ba998e5c56b99031a875bc2056d231bdd
zhang617-creat <67083621+zhang617-creat@users.noreply.github.com>
2022-06-17 21:53:47 +0800

    Update GitTest.java

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -13,7 +13,7 @@ public class GitTest {
         System.out.println("hello git1");
         System.out.println("master test");
         System.out.println("hot-fix");
-
+        System.out.println("push test");
 System.out.println("hot-fix1111");
 
     }
